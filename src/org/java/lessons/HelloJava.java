a36828ff3ee2ead8ca3722630fa96976a05cb888
FrancescoGuggino <landongu@gmail.com>
2023-07-11 12:34:53 +0200

    setup project e classe HelloJava

---
+++ src/org/java/lessons/HelloJava.java
@@ -0,0 +1,14 @@
+package org.java.lessons;
+
+public class HelloJava {
+    // tutto il codice va scritto nelle parentesi graffe della classe
+
+    /*
+    * commento su più righe
+    * */
+
+    public static void main(String[] args) {
+        System.out.println("This is my first Java program!");
+        System.out.println("Bye");
+    }
+}
